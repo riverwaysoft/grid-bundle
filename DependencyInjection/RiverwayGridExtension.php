575622b7fe7133e79a03a92d24465bd7a68417a1
Sorokin Victor <st-agrail@yandex.ru>
2017-09-14 19:18:04 +0300

    psr-4 fix

--- DependencyInjection/RiverwayGridExtension.php
+++ DependencyInjection/RiverwayGridExtension.php
@@ -28,7 +28,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class GridExtension extends Extension
+class RiverwayGridExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
